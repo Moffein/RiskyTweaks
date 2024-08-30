3f74a7c42eb03c80d421a9d63cb861934e7bf3c1
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-30 00:28:46 -0700

    Treebot stuff

--- RiskyTweaks/SneedUtils.cs
+++ RiskyTweaks/SneedUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RoR2;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine.AddressableAssets;
---
+++ RiskyTweaks/Tweaks/Survivors/Treebot/SelfDamageTweaks.cs
@@ -0,0 +1,44 @@
+﻿using MonoMod.Cil;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.Treebot
+{
+    public class SelfDamageTweaks : TweakBase<SelfDamageTweaks>
+    {
+        public override string ConfigCategoryString => "Survivors - REX";
+
+        public override string ConfigOptionName => "(Server-Side) Seed Barrage Self-Damage Consistency";
+
+        public override string ConfigDescriptionString => "Seed Barrage self-damage is affected by armor, like the other 2 plant skills.";
+
+        public override bool StopLoadOnConfigDisable => true;
+
+        protected override void ApplyChanges()
+        {
+            IL.EntityStates.Treebot.Weapon.FireMortar2.OnEnter += FireMortar2_OnEnter;
+        }
+
+        private void FireMortar2_OnEnter(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            if (c.TryGotoNext(
+                 x => x.MatchCallvirt<HealthComponent>("TakeDamage")
+                ))
+            {
+                c.EmitDelegate<Func<DamageInfo, DamageInfo>>((damageInfo) =>
+                {
+                    damageInfo.damageType &= ~DamageType.BypassArmor;
+                    return damageInfo;
+                });
+            }
+            else
+            {
+                UnityEngine.Debug.LogError("RiskyTweaks: TreeBot SelfDamageTweaks IL Hook failed");
+            }
+        }
+    }
+}
