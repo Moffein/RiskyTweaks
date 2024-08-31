c6a9bbde121cd3b57ced124c22ab533e39b4d62e
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-30 22:22:25 -0700

    Bandit Autofire

---
+++ RiskyTweaks/Tweaks/Survivors/Bandit2/PrimaryAutoFire.cs
@@ -0,0 +1,56 @@
+﻿using EntityStates;
+using RoR2.Skills;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+
+namespace RiskyTweaks.Tweaks.Survivors.Bandit2
+{
+    public class PrimaryAutoFire : TweakBase<PrimaryAutoFire>
+    {
+        public override string ConfigCategoryString => "Survivors - Bandit";
+
+        public override string ConfigOptionName => "(Client-Side) Primary Autofire";
+
+        public override string ConfigDescriptionString => "Primaries automatically fire when holding down the button.";
+
+        protected override void ApplyChanges()
+        {
+            ReloadSkillDef shotgun = Addressables.LoadAssetAsync<ReloadSkillDef>("RoR2/Base/Bandit2/FireShotgun2.asset").WaitForCompletion();
+            shotgun.mustKeyPress = false;
+            shotgun.interruptPriority = EntityStates.InterruptPriority.Skill;
+
+            ReloadSkillDef rifle = Addressables.LoadAssetAsync<ReloadSkillDef>("RoR2/Base/Bandit2/Bandit2Blast.asset").WaitForCompletion();
+            rifle.mustKeyPress = false;
+            rifle.interruptPriority = EntityStates.InterruptPriority.Skill;
+
+
+            On.EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase.GetMinimumInterruptPriority += Bandit2FirePrimaryBase_GetMinimumInterruptPriority;
+            On.EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase.OnEnter += Bandit2FirePrimaryBase_OnEnter;
+        }
+
+        private void Bandit2FirePrimaryBase_OnEnter(On.EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase.orig_OnEnter orig, EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase self)
+        {
+            /*if (fireMode == BanditFireMode.Tap)
+            {
+                self.minimumBaseDuration = autoFireDuration;
+            }
+            else
+            {
+                self.minimumBaseDuration = burstFireDuration;
+            }*/
+            self.minimumBaseDuration = 0.3f;
+            orig(self);
+        }
+
+        private InterruptPriority Bandit2FirePrimaryBase_GetMinimumInterruptPriority(On.EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase.orig_GetMinimumInterruptPriority orig, EntityStates.Bandit2.Weapon.Bandit2FirePrimaryBase self)
+        {
+            if (self.fixedAge <= self.minimumDuration && self.inputBank.skill1.wasDown)
+            {
+                return InterruptPriority.PrioritySkill;
+            }
+            return InterruptPriority.Any;
+        }
+    }
+}
