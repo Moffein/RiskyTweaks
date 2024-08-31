adef621817375a43c553a25e85eaa473287ebf55
Moffein <22742925+Moffein@users.noreply.github.com>
2024-08-31 02:03:51 -0700

    Nerf Fog

---
+++ RiskyTweaks/Tweaks/Stages/VoidRaid/NerfFog.cs
@@ -0,0 +1,38 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+
+namespace RiskyTweaks.Tweaks.Stages.VoidRaid
+{
+    public class NerfFog : TweakBase<NerfFog>
+    {
+        public override string ConfigCategoryString => "Stages - Planetarium";
+
+        public override string ConfigOptionName => "(Server-Side) Nerf Fog";
+
+        public override string ConfigDescriptionString => "Nerfs fog damage settings to be consistent with Void Fields.";
+
+        private SceneDef voidRaidScene = Addressables.LoadAssetAsync<SceneDef>("RoR2/DLC1/voidraid/voidraid.asset").WaitForCompletion();
+
+        protected override void ApplyChanges()
+        {
+            On.RoR2.FogDamageController.Start += FogDamageController_Start;
+        }
+
+        //Bruteforce way of doing this
+        private void FogDamageController_Start(On.RoR2.FogDamageController.orig_Start orig, FogDamageController self)
+        {
+            orig(self);
+            if (SceneCatalog.GetSceneDefForCurrentScene() == voidRaidScene)
+            {
+                //Debug.Log("HP% per second: " + self.healthFractionPerSecond);   //0.01
+                //Debug.Log("HP% Ramping per second: " + self.healthFractionRampCoefficientPerSecond + "\n");    //0.1
+
+                self.healthFractionPerSecond = 0.025f;
+                self.healthFractionRampCoefficientPerSecond = 0f;
+            }
+        }
+    }
+}
---
+++ RiskyTweaks/Tweaks/Stages/VoidStage/NerfFog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Stages.VoidStage
+{
+    public class NerfFog : TweakBase<NerfFog>
+    {
+        public override string ConfigCategoryString => "Stages - Void Locus";
+
+        public override string ConfigOptionName => "(Server-Side) Nerf Fog";
+
+        public override string ConfigDescriptionString => "Nerfs fog damage settings to be consistent with Void Fields.";
+
+        protected override void ApplyChanges()
+        {
+            On.RoR2.VoidStageMissionController.RequestFog += VoidStageMissionController_RequestFog;
+        }
+
+        private RoR2.VoidStageMissionController.FogRequest VoidStageMissionController_RequestFog(On.RoR2.VoidStageMissionController.orig_RequestFog orig, RoR2.VoidStageMissionController self, RoR2.IZone zone)
+        {
+            if (self.fogDamageController)
+            {
+                self.fogDamageController.healthFractionPerSecond = 0.025f;//0.05
+                self.fogDamageController.healthFractionRampCoefficientPerSecond = 0f;//0.1
+            }
+            return orig(self, zone);
+        }
+    }
+}
