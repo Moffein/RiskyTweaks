82d088e6f5271a1644d91e73c4dc15b8da8640c0
Moffein <22742925+Moffein@users.noreply.github.com>
2024-09-07 12:30:00 -0700

    Removed scorchling iframes

---
+++ RiskyTweaks/Tweaks/Enemies/Scorchling/RemoveIframes.cs
@@ -0,0 +1,34 @@
+﻿using MonoMod.Cil;
+using RoR2;
+using System;
+using UnityEngine;
+
+namespace RiskyTweaks.Tweaks.Enemies.Scorchling
+{
+    public class RemoveIframes : TweakBase<RemoveIframes>
+    {
+        public override string ConfigCategoryString => "Enemies - Scorch Wurm";
+
+        public override string ConfigOptionName => "(Server-Side) Remove Invulnerability Frames";
+
+        public override string ConfigDescriptionString => "Removes invulnerability when burrowing.";
+
+        protected override void ApplyChanges()
+        {
+            IL.ScorchlingController.Burrow += ScorchlingController_Burrow;
+        }
+
+        private void ScorchlingController_Burrow(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(x => x.MatchCallvirt(typeof(CharacterBody), "AddBuff")))
+            {
+                c.EmitDelegate<Func<BuffIndex, BuffIndex>>(orig => BuffIndex.None);
+            }
+            else
+            {
+                Debug.LogError("RiskyMod: Scorchling IL Hook failed");
+            }
+        }
+    }
+}
