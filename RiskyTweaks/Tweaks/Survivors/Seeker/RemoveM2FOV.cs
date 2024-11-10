a7a2b4d54605306b6e054ccbc1128e038f3f7121
Moffein <22742925+Moffein@users.noreply.github.com>
2024-11-09 23:36:24 -0600

    Seeker M2 FoV

---
+++ RiskyTweaks/Tweaks/Survivors/Seeker/RemoveM2FOV.cs
@@ -0,0 +1,38 @@
+﻿using MonoMod.Cil;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace RiskyTweaks.Tweaks.Survivors.Seeker
+{
+    public class RemoveM2FoV : TweakBase<RemoveM2FoV>
+    {
+        public override string ConfigCategoryString => "Survivors - Seeker";
+
+        public override string ConfigOptionName => "(Client-Side) Remove M2 FoV Modifier";
+
+        public override string ConfigDescriptionString => "Prevents FoV from changing when using Seeker's M2.";
+
+        public override bool StopLoadOnConfigDisable => false;
+
+        protected override void ApplyChanges()
+        {
+            IL.EntityStates.Seeker.UnseenHand.FixedUpdate += UnseenHand_FixedUpdate;
+        }
+
+        private void UnseenHand_FixedUpdate(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+            if (c.TryGotoNext(MoveType.After, x => x.MatchCall(typeof(EntityStates.EntityState), "get_cameraTargetParams")))
+            {
+                c.EmitDelegate<Func<CameraTargetParams, CameraTargetParams>>(orig => null);
+            }
+            else
+            {
+                Debug.LogError("RiskyTweaks: Seeker RemoveM2FoV IL Hook failed.");
+            }
+        }
+    }
+}
