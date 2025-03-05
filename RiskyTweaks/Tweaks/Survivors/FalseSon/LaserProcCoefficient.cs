341d1c84ff81717142a6e8d8c8618651a0205f1f
Moffein <22742925+Moffein@users.noreply.github.com>
2025-03-04 18:45:39 -0600

    false son proc coefficient stuff

---
+++ RiskyTweaks/Tweaks/Survivors/FalseSon/LaserProcCoefficient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.FalseSon
+{
+    public class LaserProcCoefficient : TweakBase<LaserProcCoefficient>
+    {
+        public override string ConfigCategoryString => "Survivors - False Son";
+
+        public override string ConfigOptionName => "(Client-Side) Laser of the Father - Increase Proc Coefficient";
+
+        public override string ConfigDescriptionString => "Increases Laser of the Father proc coefficient to 1.";
+
+        protected override void ApplyChanges()
+        {
+            SneedUtils.SetAddressableEntityStateField("RoR2/DLC2/FalseSon/EntityStates.FalseSon.LaserFatherCharged.asset", "procCoefficientPerTick", "1");
+        }
+    }
+}
---
+++ RiskyTweaks/Tweaks/Survivors/FalseSon/SlamProcCoefficient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.FalseSon
+{
+    public class SlamProcCoefficient : TweakBase<SlamProcCoefficient>
+    {
+        public override string ConfigCategoryString => "Survivors - False Son";
+
+        public override string ConfigOptionName => "(Client-Side) Club of the Forsaken - Reduce Proc Coefficient";
+
+        public override string ConfigDescriptionString => "Reduces fully-charged Club of the Forsaken proc coefficient to 1.";
+
+        protected override void ApplyChanges()
+        {
+            SneedUtils.SetAddressableEntityStateField("RoR2/DLC2/FalseSon/EntityStates.FalseSon.ChargedClubSwing.asset", "unchargedBlastProcCoefficient", "1");
+            SneedUtils.SetAddressableEntityStateField("RoR2/DLC2/FalseSon/EntityStates.FalseSon.ChargedClubSwing.asset", "blastProcCoefficient", "1");
+        }
+    }
+}
