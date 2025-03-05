5f0da2e4796a2365f9ff4a480211c37f626070ec
Moffein <22742925+Moffein@users.noreply.github.com>
2025-03-04 18:09:43 -0600

    updated for latest game updated, reverted dumb void fields changes

---
+++ RiskyTweaks/Tweaks/Stages/Arena/NerfFog.cs
@@ -0,0 +1,29 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+
+namespace RiskyTweaks.Tweaks.Stages.Arena
+{
+    public class NerfFog : TweakBase<NerfFog>
+    {
+        public override string ConfigCategoryString => "Stages - Void Fields";
+
+        public override string ConfigOptionName => "(Server-Side) Remove SotS Part 2 Ramping Fog Damage";
+
+        public override string ConfigDescriptionString => "Reverts SotS Phase 2 teamwide fog damage penalties.";
+
+        private SceneDef arenaScene = Addressables.LoadAssetAsync<SceneDef>("RoR2/Base/arena/arena.asset").WaitForCompletion();
+
+        protected override void ApplyChanges()
+        {
+            On.RoR2.ArenaMissionController.UpdateFogDamageVoidFieldsDamageMultiplier += ArenaMissionController_UpdateFogDamageVoidFieldsDamageMultiplier;
+        }
+
+        private void ArenaMissionController_UpdateFogDamageVoidFieldsDamageMultiplier(On.RoR2.ArenaMissionController.orig_UpdateFogDamageVoidFieldsDamageMultiplier orig, ArenaMissionController self, int voidFieldsDamageMultiplier)
+        {
+            return;
+        }
+    }
+}
---
+++ RiskyTweaks/Tweaks/Stages/Arena/RemoveItemGain.cs
@@ -0,0 +1,30 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.AddressableAssets;
+
+namespace RiskyTweaks.Tweaks.Stages.Arena
+{
+    public class RemoveItemGain : TweakBase<RemoveItemGain>
+    {
+        public override string ConfigCategoryString => "Stages - Void Fields";
+
+        public override string ConfigOptionName => "(Server-Side) Remove SotS Part 2 Item Gain";
+
+        public override string ConfigDescriptionString => "Reverts the SotS Part 2 change that makes enemies gain items while you are outside the bubble.";
+
+        private SceneDef arenaScene = Addressables.LoadAssetAsync<SceneDef>("RoR2/Base/arena/arena.asset").WaitForCompletion();
+
+        protected override void ApplyChanges()
+        {
+            On.EntityStates.Missions.Arena.NullWard.Active.HandlePenalties += Active_HandlePenalties;
+        }
+
+        private void Active_HandlePenalties(On.EntityStates.Missions.Arena.NullWard.Active.orig_HandlePenalties orig, EntityStates.Missions.Arena.NullWard.Active self, ref float percentOfPlayersInRadius)
+        {
+            self.timeSincePreviousItemPunishment = 0f;
+            orig(self, ref percentOfPlayersInRadius);
+        }
+    }
+}
