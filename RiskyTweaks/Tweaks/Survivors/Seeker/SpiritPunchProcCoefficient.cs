a86422ff6b532b5b3a14c3fe8037033e466dd498
Moffein <22742925+Moffein@users.noreply.github.com>
2025-04-04 22:11:12 -0500

    I am going to become the joker.

---
+++ RiskyTweaks/Tweaks/Survivors/Seeker/SpiritPunchProcCoefficient.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using RoR2;
+using RoR2.Projectile;
+
+namespace RiskyTweaks.Tweaks.Survivors.Seeker
+{
+    public class SpiritPunchProcCoefficient : TweakBase<SpiritPunchProcCoefficient>
+    {
+        public override string ConfigCategoryString => "Survivors - Seeker";
+
+        public override string ConfigOptionName => "(Server-Side) Spirit Punch Proc Coefficient";
+
+        public override string ConfigDescriptionString => "Fixes Spirit Punch 3rd hit having a lower proc coefficient.";
+
+        protected override void ApplyChanges()
+        {
+            GameObject projectilePrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC2/Seeker/SpiritPunchFinisherProjectile.prefab").WaitForCompletion();
+            ProjectileImpactExplosion pie = projectilePrefab.GetComponent<ProjectileImpactExplosion>();
+            pie.blastProcCoefficient = 1f;
+        }
+    }
+}
