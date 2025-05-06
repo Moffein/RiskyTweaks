ecc3c1b9acecce13cd09cd137d7c621195060b7e
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-05 23:27:32 -0500

    glaze interrupt priority

---
+++ RiskyTweaks/Tweaks/Survivors/CHEF/GlazeInterrupt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.CHEF
+{
+    public class GlazeInterrupt : TweakBase<GlazeInterrupt>
+    {
+        public override string ConfigCategoryString => "Survivors - CHEF";
+
+        public override string ConfigOptionName => "(Client-Side) Glaze InterruptPriority";
+
+        public override string ConfigDescriptionString => "Glaze is no longer interrupted by Sear.";
+
+        protected override void ApplyChanges()
+        {
+            On.EntityStates.Chef.Glaze.GetMinimumInterruptPriority += Glaze_GetMinimumInterruptPriority;
+        }
+
+        private EntityStates.InterruptPriority Glaze_GetMinimumInterruptPriority(On.EntityStates.Chef.Glaze.orig_GetMinimumInterruptPriority orig, EntityStates.Chef.Glaze self)
+        {
+            return (self.grenadeCount < EntityStates.Chef.Glaze.grenadeCountMax) ? EntityStates.InterruptPriority.Pain : EntityStates.InterruptPriority.PrioritySkill;
+        }
+    }
+}
