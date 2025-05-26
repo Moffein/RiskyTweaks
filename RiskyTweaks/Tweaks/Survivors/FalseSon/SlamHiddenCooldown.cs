a44fefe77d6383276f19fd7702cde7e565f32ac0
Moffein <22742925+Moffein@users.noreply.github.com>
2025-05-26 17:31:00 -0500

    false son hidden slam cooldown removal

---
+++ RiskyTweaks/Tweaks/Survivors/FalseSon/SlamHiddenCooldown.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.FalseSon
+{
+    public class SlamHiddenCooldown : TweakBase<SlamHiddenCooldown>
+    {
+        public override string ConfigCategoryString => "Survivors - False Son";
+
+        public override string ConfigOptionName => "(Client-Side) Club of the Forsaken - Remove Slam Cooldown";
+
+        public override string ConfigDescriptionString => "Removes hidden non-scaling cooldown from Club of the Forsaken's slam.";
+
+        protected override void ApplyChanges()
+        {
+            SneedUtils.SetAddressableEntityStateField("RoR2/DLC2/FalseSon/EntityStates.FalseSon.ClubSwing.asset", "secondaryAltCooldownDuration", "0");
+        }
+    }
+}
