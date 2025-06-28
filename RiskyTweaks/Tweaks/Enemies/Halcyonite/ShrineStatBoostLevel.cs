31b478b8bf8ff76a0411bf38461ec24992d30afb
Moffein <22742925+Moffein@users.noreply.github.com>
2025-06-28 11:47:56 -0500

    capped shrine stat boosts for halcyonite

---
+++ RiskyTweaks/Tweaks/Enemies/Halcyonite/ShrineStatBoostLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RiskyTweaks.Tweaks.Enemies.Halcyonite
+{
+    public class ShrineStatBoostLevel : TweakBase<ShrineStatBoostLevel>
+    {
+        public override string ConfigCategoryString => "Enemies - Halcyonite";
+
+        public override string ConfigOptionName => "(Server-Side) Shrine Stat Boost Cap";
+
+        public override string ConfigDescriptionString => "Caps shrine stat boosts at +20 (12x damage, 4x hp).";
+
+        protected override void ApplyChanges()
+        {
+            On.RoR2.CombatDirector.HalcyoniteShrineActivation += CombatDirector_HalcyoniteShrineActivation;
+        }
+
+        private void CombatDirector_HalcyoniteShrineActivation(On.RoR2.CombatDirector.orig_HalcyoniteShrineActivation orig, RoR2.CombatDirector self, float monsterCredit, RoR2.DirectorCard chosenDirectorCard, int difficultyLevel, UnityEngine.Transform shrineTransform)
+        {
+            orig(self, monsterCredit, chosenDirectorCard, Mathf.Min(20, difficultyLevel), shrineTransform);
+        }
+    }
+}
