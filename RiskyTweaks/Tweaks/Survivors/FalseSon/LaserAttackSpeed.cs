3f9e193b88030793db7988f5dc184bd1cf65ae86
Moffein <22742925+Moffein@users.noreply.github.com>
2026-05-16 20:00:28 -0700

    False Son laser attack speed

---
+++ RiskyTweaks/Tweaks/Survivors/FalseSon/LaserAttackSpeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskyTweaks.Tweaks.Survivors.FalseSon
+{
+    public class LaserAttackSpeed : TweakBase<LaserAttackSpeed>
+    {
+        public override string ConfigCategoryString => "Survivors - False Son";
+
+        public override string ConfigOptionName => "(Client-Side) Laser of the Father - Attack Speed Scaling";
+
+        public override string ConfigDescriptionString => "Laser of the Father properly scales tickrate with attack speed.";
+
+        protected override void ApplyChanges()
+        {
+            On.EntityStates.FalseSon.LaserFatherCharged.OnEnter += LaserFatherCharged_OnEnter;
+        }
+
+        private void LaserFatherCharged_OnEnter(On.EntityStates.FalseSon.LaserFatherCharged.orig_OnEnter orig, EntityStates.FalseSon.LaserFatherCharged self)
+        {
+            orig(self);
+            self.fireFrequency = EntityStates.FalseSon.LaserFatherCharged.baseFireFrequency * self.attackSpeedStat;
+        }
+    }
+}
